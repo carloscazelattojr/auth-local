33af630e9409da4d79ccf622b16e9cb85b29e1e5
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-10-21 11:31:03 +0000

    add fingerprint and facialrecognition IOS

--- App.tsx
+++ App.tsx
@@ -1,13 +1,48 @@
-import { useState } from "react";
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, Button, Text, View } from "react-native";
 import { styles } from "./styles";
+
+import * as LocalAuthentication from "expo-local-authentication";
 
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  async function verifyAvaiableAuthentication() {
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+    console.log(compatible);
+
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    console.log(types);
+    console.log(
+      types.map((type) => LocalAuthentication.AuthenticationType[type])
+    );
+  }
+
+  async function handleAuthentication() {
+    const isBiometricEnrolled = await LocalAuthentication.isEnrolledAsync();
+
+    if (!isBiometricEnrolled) {
+      return Alert.alert(
+        "Login",
+        "Nenhuma biometria encontrada. Cadastre no dispositivo"
+      );
+    }
+
+    const auth = await LocalAuthentication.authenticateAsync({
+      promptMessage: "Login com biometria Mundo Azul",
+      fallbackLabel: "Biometria não reconhecida",
+    });
+    setIsAuthenticated(auth.success);
+  }
+
+  useEffect(() => {
+    verifyAvaiableAuthentication();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Usuário conectado: {isAuthenticated ? "SIM" : "NÃO"}</Text>
+      <Button title="Entrar" onPress={handleAuthentication} />
     </View>
   );
 }
